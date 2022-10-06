8974fc944c74795b35c36148a0d57d104ae12d7f
Kevin Romero Villa <kromerov1@outlook.com>
2022-10-05 21:55:27 -0500

    Se agrego botones de signup y login al la pagina principal de Wallapop

--- Wallapop/index.js
+++ Wallapop/index.js
@@ -2,9 +2,27 @@ import { AdsListController } from "./ads-list/AdsListController.js";
 import { NotificationController } from "./notification/NotificationController.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-    const adsListElement = document.querySelector('#ads-list');
-    const notificationElement = document.querySelector('#notification')
+    const createController = () => {
+        const adsListElement = document.querySelector('#ads-list');
+        const notificationElement = document.querySelector('#notification')
+    
+        const notificationController = new NotificationController(notificationElement)
+        const adsListController = new AdsListController(adsListElement)
+    }
 
-    const notificationController = new NotificationController(notificationElement)
-    const adsListController = new AdsListController(adsListElement)
+    const handleUserLogged = () => {
+        const token = localStorage.getItem('token')
+        const userActions = document.querySelector('#user-actions')
+        if (token) {
+            userActions.innerHTML = '<a href="/createTweet.html">Crear Anuncio</a>'
+        } else {
+            userActions.innerHTML = `
+            <a href="/signup.html">Signup</a>
+            <a href="/login.html">Login</a>
+            `
+        }
+    }
+
+    createController();
+    handleUserLogged();
 })
